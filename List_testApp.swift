e4a57057ef5193df9645c0eb03371d441b47b2b3
Alexandr Kislitsin <a_kislitsin@kbtu.kz>
2026-01-20 19:03:49 +0500

    Add files via upload

---
+++ List_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_testApp.swift
+//  List_test
+//
+//  Created by Alexandr Kisslitsyn on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct List_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
